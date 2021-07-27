c4d05ce85f6883e4c013a09b488822c2478f6682
brandon <cynorthport@yahoo.com>
2021-07-26 22:58:08 -0400

    Updated

---
+++ handlers/home.js
@@ -0,0 +1,32 @@
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
+const cats = require("../data/cats.json");
+
+module.exports = (req, res) => {
+	const pathname = url.parse(req.url).pathname;
+	if (pathname === "/" && req.method === "GET") {
+		let filepath = path.normalize(
+			path.join(__dirname, "../views/home/index.html")
+		);
+		fs.readFile(filepath, (err, data) => {
+			if (err) {
+				console.log(err);
+				red.writeHead(404, {
+					"content-type": "text/plain",
+				});
+
+				res.write(404);
+				res.end();
+				return;
+			}
+			res.writeHead(200, {
+				"content-type": "text/html",
+			});
+			res.write(data);
+			res.end();
+		});
+	} else {
+		return true;
+	}
+};
